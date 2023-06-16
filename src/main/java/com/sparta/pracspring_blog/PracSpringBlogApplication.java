225210b8c76d60e2860bf3f8cefa2d6c2d895ceb
song eojin <feshed528@gmail.com>
2023-06-16 10:45:34 +0900

    init commit

---
+++ src/main/java/com/sparta/pracspring_blog/PracSpringBlogApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.pracspring_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracSpringBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracSpringBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/pracspring_blog/PracSpringBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.pracspring_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracSpringBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
