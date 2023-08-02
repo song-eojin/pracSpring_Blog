1e11b77c6bfcc7495250801c6e3d77bb4826eecb
song eojin <feshed528@gmail.com>
2023-08-03 03:36:11 +0900

    [플러스주차 복습과제] 로그인 API 보완
    - 7/26 - 로그인 API
        - 닉네임, 비밀번호를 request에서 전달받기
        - 로그인 버튼을 누른 경우 아이디(username)와 비밀번호가 데이터베이스에 등록됐는지 확인한 뒤, 하나라도 맞지 않는 정보가 있다면 "아이디 또는 패스워드를 확인해주세요."라는 에러 메세지를 response에 포함하기
        - 로그인 성공 시, 로그인에 성공한 유저의 정보를 JWT를 활용하여 클라이언트에게 Cookie로 전달하기

--- src/main/java/com/sparta/pracspring_blog/common/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/sparta/pracspring_blog/common/jwt/JwtAuthenticationFilter.java
@@ -1,6 +1,7 @@
 package com.sparta.pracspring_blog.common.jwt;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparta.pracspring_blog.common.dto.MsgResponseDto;
 import com.sparta.pracspring_blog.common.security.UserDetailsImpl;
 import com.sparta.pracspring_blog.user.dto.UserRequestDto;
 import com.sparta.pracspring_blog.user.entity.UserRoleEnum;
@@ -17,6 +18,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 import java.io.IOException;
 
+/* JwtAuthenticationFilter는 JWT (JSON Web Token) 기반의 인증을 처리하는 필터 */
 @Slf4j(topic = "JwtAuthenticationFilter : 로그인 인증 및 생성된 JWT 토큰을 응답 헤더에 추가하는 역할의 필터")
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     private final JwtTokenProvider jwtTokenProvider;
@@ -51,10 +53,21 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         String username = ((UserDetailsImpl) authResult.getPrincipal()).getUsername();
         UserRoleEnum role = ((UserDetailsImpl) authResult.getPrincipal()).getUser().getRole();
 
+
+        /* 로그인 성공 시, 로그인에 성공한 유저의 정보를 JWT를 활용하여 클라이언트에게 Cookie로 전달하기 */
+
+        // JWT 토큰을 생성하고 관리하는 클래스를 사용하여 토큰 생성
         String token = jwtTokenProvider.createToken(username, role);
+
+        // JWT 토큰을 HTTP 응답 헤더에 추가하고, 이를 통해 클라이언트는 로그인에 성공한 사용자를 식별할 수 있는 토큰을 받게 된다.
         response.addHeader(JwtTokenProvider.AUTHORIZATION_HEADER, token);
-        response.setStatus(HttpStatus.OK.value()); // 로그인 성공 상태 코드
-        response.setContentType(MediaType.APPLICATION_JSON_VALUE); // 응답의 Content-Type을 JSON으로 설정
+
+        // 로그인 성공 상태 코드로 로그인이 성공적으로 처리되었음을 클라이언트에게 알려주는 역할
+        response.setStatus(HttpStatus.OK.value());
+
+        // HTTP 응답의 Content-Type을 JSON으로 설정
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
 
         // "로그인 성공" 문자열을 응답
         String successMessage = "로그인 성공";
@@ -63,12 +76,31 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     }
 
 
+    /* JWT가 유효하지 않거나 인증에 실패한 경우 */
+
+        // HttpServletRequest request : 클라이언트로부터 온 HTTP 요청 객체
+        // HttpServletResponse response : 서버가 클라이언트로 보낼 HTTP 응답 객체
+        // AuthenticationException failed: 인증에 실패한 예외 정보를 담고 있는 객체
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException {
+        response.setStatus(401); //HTTP 응답 상태 코드를 401(Unauthorized)로 설정, 이는 인증이 실패한 상태임을 클라이언트에게 알려주는 역할을 한다.
+
         log.error("로그인 실패");
-        String failureMessage = "로그인 실패";
-        response.setStatus(401);
+
+        String failureMessage = "로그인 실패"; //로그인 실패"라는 문자열을 failureMessage 변수에 저장
         response.getWriter().write(failureMessage);
-        response.getWriter().flush();
+        returnResponse(response, "아이디 또는 패스워드를 확인해주세요.");
+        response.getWriter().flush(); //응답을 완전히 보내고 버퍼를 비우기
+        response.getWriter().close();
+    }
+
+
+    public void returnResponse(HttpServletResponse response, String message) throws IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+        MsgResponseDto msgResponseDto = new MsgResponseDto(message, response.getStatus());
+        ObjectMapper objectMapper = new ObjectMapper();
+        String result = objectMapper.writeValueAsString(msgResponseDto);
+        response.getWriter().write(result);
     }
 }
